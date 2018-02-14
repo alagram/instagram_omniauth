85fc21baa3fa414a8df7466e680f5e527e0ba3ce
Albert Agram <albert.agram@gmail.com>
2018-02-14 23:46:25 +0000

    ActivityController and spec

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,4 +1,20 @@
 class ActivityController < ApplicationController
   def mine
+    client = Instagram.client(access_token: current_user.settings["token"])
+
+    recent_media = client.user_recent_media
+    album = [].concat(recent_media)
+
+    max_id = recent_media.pagination.next_max_id
+
+    while !(max_id.to_s.empty?) do
+      response = client.user_recent_media(:max_id => max_id)
+      max_id = response.pagination.next_max_id
+      album.concat(response)
+    end
+
+    @album = album
+
+    @paginatable_array = Kaminari.paginate_array(@album).page(params[:page]).per(6)
   end
 end
--- spec/controllers/activity_controller_spec.rb
+++ spec/controllers/activity_controller_spec.rb
@@ -2,10 +2,32 @@ require 'rails_helper'
 
 RSpec.describe ActivityController, type: :controller do
 
+
   describe "GET #mine" do
-    it "returns http success" do
+    user =
+      FactoryBot.create(
+        :user,
+        uid: '5702001256',
+        settings: { token: '5702001256.a2164f4.32d510f6063c4367aa2ed85441ab8054' }
+      )
+
+    login_user(user)
+
+    before do
       get :mine
-      expect(response).to have_http_status(:success)
+    end
+
+    it "returns http success" do
+      VCR.use_cassette('instagram/client') do
+        expect(response).to have_http_status(:success)
+      end
+
+    end
+
+    it "sets album instance variable" do
+      VCR.use_cassette('instagram/client') do
+        expect(assigns(:album)).to be_truthy
+      end
     end
   end
 
